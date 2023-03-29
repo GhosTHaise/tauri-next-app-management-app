43dffe2bf9177e10f89447d0e4d51abcb07e1322
GhosTHaise <Ghostrex2@gmail.com>
2023-03-29 20:51:33 +0300

    Build hero section

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import styles from "./Hero.module.css";
+import Image from 'next/image';
 
 const Hero = () => {
   return (
     <div className={styles.weather_app}>
         <div className={styles.container}>
-            <h3>
+            <h3 className={styles.brand}>
                 The weather
             </h3>
+            <div>
+                <h1 className={styles.temp}>
+                    16&#176;
+                </h1>
+                <div className={styles.city_time}>
+                    <h1 className={styles.name}>London</h1>
+                    <small>
+                        <span className={styles.time}>06:09</span>
+                        -
+                        <span className={styles.date}>Monday Sep 19</span>
+                    </small>
+                </div>
+                <div className={styles.weather}>
+                    <Image 
+                        src='/Weather/day/113.png'
+                        className={styles.icon}
+                        alt="icon"
+                        width={50}
+                        height={50}
+                    />
+                    <span className={styles.condition}>
+                        Cloudy
+                    </span>
+                </div>
+            </div>
         </div>
     </div>
   )
